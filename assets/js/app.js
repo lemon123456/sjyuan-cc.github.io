bf13010714b94916c2e516a75eb5f2c914a5bb9b
Shenjian YUAN <sjyuan@thoughtworks.com>
2016-03-12 19:06:22 +0800

    restyle logo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-
-    //// DropCap.js
-    //var dropcaps = document.querySelectorAll(".dropcap");
-    //window.Dropcap.layout(dropcaps, 2);
-    //
-    //// Responsive-Nav
-    //var nav = responsiveNav(".nav-collapse");
-    //
-    //// Round Reading Time
-    //$(".time").text(function (index, value) {
-    //  return Math.round(parseFloat(value));
-    //});
     backToTop();
-
 });
 
 function backToTop() {
